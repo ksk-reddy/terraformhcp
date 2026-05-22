176b58115313a7da4a9c7dd2edc62c142970485b
Sunil Kumar Reddy <kskreddy1989@gmail.com>
2026-05-23 00:40:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ region = "us-east-1"
 resource "aws_instance" "Ansible_server" {
   ami           = "ami-02b2c1b57c5105166"
   instance_type = "t3.micro"
-  key_name      = "Ansible"
   tags = {
     Name = "Ansible_server"
   }
